7cabfda25496b8f8905e32eb2c630c4e37fdfc1f
cparpaillon <camille.grison@gmail.com>
2020-01-21 15:19:43 +0100

    add button switch light & dark theme

--- trusk-tracker-test-front/src/App.js
+++ trusk-tracker-test-front/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
-import { LightTheme, BaseProvider } from "baseui";
+import { LightTheme, DarkTheme, BaseProvider } from "baseui";
+import { Button } from "baseui/button";
 
 import Tracker from "./Tracker";
 
@@ -9,10 +10,24 @@ import Layout from "./components/Layout";
 
 const engine = new Styletron();
 
+const THEME = {
+  light: "light",
+  dark: "dark"
+};
+
 const App = () => {
+  const [theme, setTheme] = React.useState(THEME.light);
+
   return (
     <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
+      <BaseProvider theme={theme === THEME.light ? LightTheme : DarkTheme}>
+        <Button
+          onClick={() =>
+            setTheme(theme === THEME.light ? THEME.dark : THEME.light)
+          }
+        >
+          Light/Dark Theme
+        </Button>
         <Layout>
           <Tracker />
         </Layout>
